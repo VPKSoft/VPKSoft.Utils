bb13c4f40129aab78086837dac3621376447616d
VPKSoft <vpksoft@vpksoft.net>
2021-02-14 17:01:21 +0200

    Fixing...

--- NuGet-Push.ps1
+++ NuGet-Push.ps1
@@ -22,6 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 #>
 
+Import-Module Retry-Command
+
 Write-Output "Init NuGet push..."
 
 $output_file = ".\CryptEnvVar.exe"
