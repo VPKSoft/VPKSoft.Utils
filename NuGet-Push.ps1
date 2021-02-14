eb84489fc495f6320f03bd3ded777ae134834364
VPKSoft <vpksoft@vpksoft.net>
2021-02-14 17:14:18 +0200

    Fixed the script once more.

--- NuGet-Push.ps1
+++ NuGet-Push.ps1
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 #>
 
-Import-Module Retry-Command
+. .\Retry-Command.ps1
 
 Write-Output "Init NuGet push..."
 
